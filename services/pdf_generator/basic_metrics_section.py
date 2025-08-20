98b99f8509c13e211d491d8665416cff2968b023
Hecze <hectorzerrillo85@gmail.com>
2025-08-20 11:17:10 -0500

    refactor: standardize green color definition to HexColor format across all PDF generation modules

--- services/pdf_generator/basic_metrics_section.py
+++ services/pdf_generator/basic_metrics_section.py
@@ -7,7 +7,7 @@ from reportlab.lib.enums import TA_JUSTIFY
 white = Color(1, 1, 1)
 black = Color(0, 0, 0)
 grey = Color(0.5, 0.5, 0.5)
-green = HexColor('#14b48b')
+green = HexColor('#0c846c')
 red = Color(1, 0, 0)
 blue = Color(0, 0, 1)
 
